98eb67176605ed98f2fff4c8fa420daab6f3c8cd
Saulo de Tarso <de_tarso7@hotmail.com>
2023-04-17 14:22:34 -0300

    Criando validação do formulário

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,7 +1,17 @@
+import { useState } from 'react'
 import Box from '../UI/Box'
 import styles from './footer.module.scss'
 
 const Footer = () => {
+
+    const [nameValue, setNameValue] = useState('')
+    const [isTouched, setIsTouched] = useState(false)
+
+    const isFieldValid = nameValue !== ''
+    const isputHasError = isTouched && !isFieldValid
+
+    const isFormValid = !isputHasError && isTouched
+
     return(
        <footer className={styles['footer-wrapper']}>
         <div className={styles.footer}>
@@ -12,7 +22,16 @@ const Footer = () => {
             <form>
                 <div className="field">
                     <label htmlFor="name">Nome</label>
-                    <input type="text" id="name" name="name"/>
+                    <input 
+                    type="text" 
+                    id="name" 
+                    name="name" 
+                    onChange={(e)=> setNameValue(e.target.value)}
+                    onBlur={() => setIsTouched(true)}
+                    />
+                    {isputHasError &&
+                        <span className="error">Ops... Falta preencher o campo!</span>
+                    }
                 </div>
                 <div className="field">
                     <label htmlFor="email">E-mail</label>
@@ -22,7 +41,7 @@ const Footer = () => {
                     <label htmlFor="text">Mensagem</label>
                     <textarea id="name" name="name" rows={10}/>
                 </div>
-                <button className={"btn btn-primary"}>Enviar</button>
+                <button className={"btn btn-primary"} disabled={!isFormValid}>Enviar</button>
             </form>
             <span className={styles.copy}>Todos os direitos reservados</span>
         </div>
