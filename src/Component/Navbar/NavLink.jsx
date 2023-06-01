ffe313ebc83e8215a94a24eb47c4845d74f2172c
Saulo de Tarso <de_tarso7@hotmail.com>
2023-06-01 14:26:08 -0300

    Desabilitando ESLint

--- src/Component/Navbar/NavLink.jsx
+++ src/Component/Navbar/NavLink.jsx
@@ -3,6 +3,7 @@ import { Link } from 'next/link'
 
 const NavLink = ({text}) => {
     return(
+             // eslint-disable-next-line @next/next/no-html-link-for-pages
             <li className={styles.item}>
                 <Link className='h6'>{text}</Link>
             </li>
